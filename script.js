bf5f6dfc8cac887132b218a7347809fc2ee2d9bc
isssbelmendez <isabelmendez919@gmail.com>
2023-08-10 20:09:03 -0600

    Agregar archivo

---
+++ script.js
@@ -0,0 +1,79 @@
+//comentario de linea
+
+/*
+comentario de bloque
+ */
+
+/*var variable = 4
+console.log(variable)
+
+var variable = 5 
+console.log(variable)
+
+let otra_variable = 10
+const pi = 3.1416*/
+
+let numero1 = 1
+let numero2 = '1'
+
+if(numero1 == numero2){
+    console.log("son iguales")
+}else{
+    console.log("no son iguales")
+}
+
+//let info = prompt("Ingresa algo")
+//alert(info)
+
+/*function funcion1(){
+    alert("ESTO ES UINA FUNCION")
+}
+
+funcion1*/
+
+//document.write("Hola")
+/*document.getElementById("parrafo1").innerHTML = "<b>Esto es una parrafo</b>"
+
+const myparrafo = document.getElementById("parrafo1")
+
+myparrafo.innerHTML = "Mi parrafo 2"
+
+const otroparrafo = document.querySelector("#parrafo2")
+otroparrafo.innerHTML = "este es el otro parrafo"*/
+
+//Array.miMetodo = "prueba"
+
+/*et miArray = []
+const objeto = {}*/
+
+
+///////////////////////////////////////EJERCICIO 1 /////////////////////////////////////////
+/*let nombre = prompt("Ingresa tu nombre")
+let edad = prompt("Ingresa tu edad")
+
+if(edad >= 18){
+    document.writeln(`!!!Bienvenido!!! ${nombre} Eres mayor de edad(${edad})`)
+}else{
+    document.writeln(`Eres menor de edad(${edad})`)
+}*/
+
+
+///////////////////////////////////////EJERCICIO 2 /////////////////////////////////////////
+/*let anio_de_nacimiento = prompt("Ingresa tu edad: ")
+let anio_actual = new Date().getFullYear()
+let edad = anio_actual - parseInt(anio_de_nacimiento)
+
+alert(`Tu edad es: ${edad}`)*/
+
+
+///////////////////////////////////////EJERCICIO 3 /////////////////////////////////////////
+/*var numeroSecreto= Math.floor(Math.random() * 100) +1;
+while (true) {
+  const numeroUsuario = parseInt(prompt('Ingresa un número para adivinar el número secreto:'));
+
+  if (numeroUsuario === numeroSecreto) {
+    console.log('¡Felicidades! ¡Adivinaste el número secreto!');
+  } else {
+    console.log('Número incorrecto. Intenta nuevamente.');
+  }
+}*/
